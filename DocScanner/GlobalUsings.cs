da6d3aca358da6566cca3d6f04d67660773827f7
Muhammad Zubair Khan <zubair007tanoli@gmail.com>
2025-02-23 11:52:42 +0500

    comm

--- DocScanner/GlobalUsings.cs
+++
@@ -1,6 +0,0 @@
-global using DocScanner.Data;
-global using DocScanner.PageModels;
-global using DocScanner.Pages;
-global using DocScanner.Services;
-global using DocScanner.Utilities;
-global using Fonts;
---
+++ gsmsharing/Components/FeaturedComponents.cs
@@ -0,0 +1,15 @@
+﻿using gsmsharing.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace gsmsharing.Components
+{
+    [ViewComponent(Name = "FeaturedTop")]
+    public class FeaturedComponents : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {            
+            return View();
+
+        }
+    }
+}
