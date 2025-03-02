a82499c9bbd9631ab7e65f4f1ed8a255b671d60e
Muhammad Zubair Khan <zubair007tanoli@gmail.com>
2025-03-02 23:17:34 +0500

    c

---
+++ gsmsharing/Controllers/AdminController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace gsmsharing.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Users()
+        {
+            return View();
+        }
+
+        public IActionResult PostManagement()
+        {
+            return View();
+        }
+
+        public IActionResult CommentsManagement()
+        {
+            return View();
+        }
+
+        public IActionResult CommunitiesManagement()
+        {
+            return View();
+        }
+
+        public IActionResult Categories()
+        {
+            return View();
+        }
+    }
+}
