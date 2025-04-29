4a1611acb3df67625d94f2a01c7c79dce4a35750
Muhammad Zubair Khan <zubair007tanoli@gmail.com>
2025-04-30 00:00:37 +0500

    x

---
+++ discussionspot/Controllers/AccountsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace discussionspot.Controllers
+{
+    public class AccountsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
