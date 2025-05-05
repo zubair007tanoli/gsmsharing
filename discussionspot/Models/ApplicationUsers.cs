933e5f00cb1ff172fec65ad48eb50a0df3a9b3c6
Muhammad Zubair Khan <zubair007tanoli@gmail.com>
2025-05-06 00:54:51 +0500

    j

--- discussionspot/Models/ApplicationUsers.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace discussionspot.Models
-{
-    public class ApplicationUsers : IdentityUser
-    {
-    }
-}
--- discussionspot/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace discussionspot.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
