b3eb66d60a042de117febfa47c687ced650c5043
Muhammad Zubair Khan <zubair007khan@hotmail.com>
2025-05-20 00:50:12 +0500

    mmdm

--- discussionpost.com/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace discussionpost.com.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
---
+++ discussionspot10/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using discussionspot10.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace discussionspot10.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
