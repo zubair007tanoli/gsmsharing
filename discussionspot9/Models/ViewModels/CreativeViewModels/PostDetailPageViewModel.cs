f0932ddcbcd36e65ce45f06ee0307b1cbc6f32d9
IBMS_01 <IBMS_01@DESKTOP-MI2PPUI>
2025-07-11 18:20:36 +0500

    xx

---
+++ discussionspot9/Models/ViewModels/CreativeViewModels/PostDetailPageViewModel.cs
@@ -0,0 +1,16 @@
+﻿using discussionspot9.Models.Domain;
+
+namespace discussionspot9.Models.ViewModels.CreativeViewModels
+{
+    public class PostDetailPageViewModel
+    {
+        public PostDetailViewModel Post { get; set; }
+        public Category category { get; set; } // Added this property
+        public List<CommentTreeViewModel> Comments { get; set; }
+        public string CommunitySlug { get; set; }
+        public string PostSlug { get; set; }
+        public CommunityDetailViewModel? Community { get; set; }
+        public string CategorySlug { get; set; } // Added this property
+        public List<PostCardViewModel> RelatedPosts { get; set; }
+    }
+}
